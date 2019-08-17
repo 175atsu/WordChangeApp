5b15eb08baaed3614a0a99cddba44bbb060485c7
Atsu <zatsuyou0@gmail.com>
2019-08-17 10:02:54 +0900

    最初のコミット

---
+++ WordChangeApp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WordChangeApp
+//
+//  Created by 飯野敦博 on 2019/08/17.
+//  Copyright © 2019 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var beforeText: UITextView!
+
+    @IBOutlet weak var afterTextt: UILabel!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func changeButton(_ sender: Any) {
+    }
+    
+}
+
