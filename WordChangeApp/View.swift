b149cc642606ba5af838e7dbfc2ef16674d15e65
Atsu <zatsuyou0@gmail.com>
2019-08-21 11:42:01 +0900

    オプショナルエラー

--- WordChangeApp/View.swift
+++ WordChangeApp/View.swift
@@ -10,19 +10,19 @@ import UIKit
 
 class View: UIViewController {
 
-    @IBOutlet weak var beforeText: UITextView!
-
-    @IBOutlet weak var afterTextt: UILabel!
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-    @IBAction func changeButton(_ sender: Any) {
-        var berore = beforeText.text
-    }
+//    @IBOutlet weak var beforeText: UITextView!
+//
+//    @IBOutlet weak var afterTextt: UILabel!
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//    @IBAction func changeButton(_ sender: Any) {
+//        var berore = beforeText.text
+//    }
 
 }
--- WordChangeApp/ViewController.swift
+++ WordChangeApp/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var beforeText: UITextView!
+
+    @IBOutlet weak var afterTextt: UILabel!
+
+    let rubi = RubiModel()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeButton(_ sender: Any) {
+        //var berore: String? = beforeText.text
+        if let before = beforeText.text {
+            rubi.change(word: before)
+            afterTextt.text = rubi.change(word: before)
+        }
     }
 
 }
