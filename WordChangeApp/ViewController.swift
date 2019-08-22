cc09ee1b78367b9fb55b97aeec4d532aebde006a
Atsu <zatsuyou0@gmail.com>
2019-08-23 01:52:22 +0900

    レイアウト完成

--- WordChangeApp/ViewController.swift
+++ WordChangeApp/ViewController.swift
@@ -6,24 +6,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var afterTextt: UILabel!
 
+    @IBOutlet weak var textView: UITextView!
+
     let rubi = RubiModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        beforeText.text = ""
-        view.backgroundColor = UIColor.red
-
+        //beforeText.text = ""
+        view.backgroundColor = UIColor.init(red: 240/255, green: 240/255, blue: 240/255, alpha: 100/100)
     }
-
+    
     @IBAction func changeButton(_ sender: Any) {
-        //var berore: String? = beforeText.text
         if let before = beforeText.text {
 
             rubi.change(word: before) { (result: String) -> Void in
                 self.afterTextt.text = result
             }
-//            afterTextt.text = rubi.change(word: before)
             print(afterTextt.text)
+        }
+    }
+
+    //入力画面ないしkeyboardの外を押したら、キーボードを閉じる処理
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (self.textView.isFirstResponder) {
+            self.textView.resignFirstResponder()
         }
     }
 
